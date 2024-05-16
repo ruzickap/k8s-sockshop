ce702ef617d9c544318f667fa19fce7173a28980
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-05-16 20:51:15 +0200

    fix(doc): fix k8s logo url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,40 @@
 module.exports = {
-  title: 'Kubernetes + Flagger + Flux + Istio + Sockshop',
-  description: 'Kubernetes + Flagger + Flux + Istio + Sockshop',
-  base: '/k8s-sockshop/',
+  title: "Kubernetes + Flagger + Flux + Istio + Sockshop",
+  description: "Kubernetes + Flagger + Flux + Istio + Sockshop",
+  base: "/k8s-sockshop/",
   head: [
-    ['link', { rel: 'icon', href: 'https://kubernetes.io/images/favicon.png' }]
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "https://raw.githubusercontent.com/kubernetes/kubernetes/d9a58a39b69a0eaec5797e0f7a0f9472b4829ab0/logo/logo.svg",
+      },
+    ],
   ],
   themeConfig: {
     displayAllHeaders: true,
     lastUpdated: true,
-    repo: 'ruzickap/k8s-sockshop',
-    docsDir: 'docs',
+    repo: "ruzickap/k8s-sockshop",
+    docsDir: "docs",
     editLinks: true,
-    logo: 'https://kubernetes.io/images/favicon.png',
+    logo: "https://raw.githubusercontent.com/kubernetes/kubernetes/d9a58a39b69a0eaec5797e0f7a0f9472b4829ab0/logo/logo.svg",
     nav: [
-      { text: 'Home', link: '/' },
+      { text: "Home", link: "/" },
       {
-        text: 'Links',
+        text: "Links",
         items: [
-          { text: 'Flux', link: 'https://fluxcd.io' },
-          { text: 'Flagger', link: 'https://flagger.app' }
-        ]
-      }
+          { text: "Flux", link: "https://fluxcd.io" },
+          { text: "Flagger", link: "https://flagger.app" },
+        ],
+      },
     ],
-    sidebar: [
-      '/',
-      '/part-01/',
-      '/part-02/',
-      '/part-03/'
-    ]
+    sidebar: ["/", "/part-01/", "/part-02/", "/part-03/"],
   },
   plugins: [
-    ['@vuepress/medium-zoom'],
-    ['@vuepress/back-to-top'],
-    ['reading-progress'],
-    ['smooth-scroll'],
-    ['seo']
-  ]
-}
+    ["@vuepress/medium-zoom"],
+    ["@vuepress/back-to-top"],
+    ["reading-progress"],
+    ["smooth-scroll"],
+    ["seo"],
+  ],
+};
